758e19f0b40b01f87df274bc811056adcaceeafe
Paul WOISARD <paulwoisard@gmail.com>
2025-12-24 16:58:01 +0100

    Fix nginx PHP-FPM config

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,6 +2,6 @@
 # Supprime le fichier default.conf s'il existe
 rm -f /etc/nginx/conf.d/default.conf
 # Corrige les permissions du volume monté
-chown -R www-data:www-data /var/www/html
+chown -R nginx:nginx /var/www/html
 # Démarre Nginx
 exec "$@"
