d4f159f051fcc7f4c5851a9b7bec050b537c0db4
Paul WOISARD <paulwoisard@gmail.com>
2025-12-26 23:04:40 +0100

    correction: newsletter.js

--- js/newsletter.js
+++ js/newsletter.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
             btn.disabled = true;
 
             // Envoi des données au script PHP via AJAX (Fetch)
-            fetch('/subscribe', {
+            fetch('subscribe', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                 body: 'email=' + encodeURIComponent(email)
